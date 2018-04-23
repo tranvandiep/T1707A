34fd4c65c4875b5edc7a2e4dbe87804d40defea4
TRẦN VĂN ĐIỆP <tranvandiep.it@gmail.com>
2018-04-23 17:20:42 +0700

    swing - input students

---
+++ src/thread/Student.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thread;
+
+/**
+ *
+ * @author Diep.Tran
+ */
+public class Student {
+    String username;
+    String gender;
+    String email;
+    String phoneNumber;
+
+    public Student() {
+    }
+
+    public Student(String username, String gender, String email, String phoneNumber) {
+        this.username = username;
+        this.gender = gender;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+}
